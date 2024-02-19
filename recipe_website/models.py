640e888ed67339ed34784b3e4e78ed30497d52e5
Kb-Mash <karabo.kmashile@gmail.com>
2024-02-19 10:35:14 +0200

    models: User, Recipe

---
+++ recipe_website/models.py
@@ -0,0 +1,20 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_name = db.Column(db.String(255))
+    recipe_description = db.Column(db.String(10000))
+    recipe_ingredients = db.Column(db.String(10000))
+    recipe_instructions = db.Column(db.String(10000))
+    # recipe_image = db.Column(db.String(150)) to-do - add image
+    date = db.Column(db.DateTime(timezone=True), default=db.func.now()) # default - automatically set the date and time
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    username = db.Column(db.String(150), unique=True)
+    recipes = db.relationship('Recipe') # relationship between user and recipe, one to many
